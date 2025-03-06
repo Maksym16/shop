6877290747c5c8e374d8f2b5a74b86b97d219152
Maksym <mfedorenko16@gmail.com>
2025-03-06 04:53:10 -0500

    fav icon update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ export default async function Navbar() {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
         <Link href="/" className='ml-4 flex lg:ml-0 gap-x-2'>
-          <p className='font-bold text-xl'>Store</p>
+          <p className='font-bold text-xl'>Coffee Shop</p>
         </Link>
         <MainNav data={categories}/>
         <NavbarActions />
