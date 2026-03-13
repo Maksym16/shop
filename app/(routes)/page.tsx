f4c0c4385cfbbe80fe63d2fdb2af874631ec3cb1
Maksym <mfedorenko16@gmail.com>
2026-03-12 23:37:53 -0400

    banners update

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,11 +8,11 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard('10c49c9a-a715-4ba5-a129-93cdc6dea506');
+  const billboard = await getBillboard('f166623e-c5c4-48f1-b7a9-d41e88d8e2de');
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        <Billboard data={billboard} hideText />
 
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
