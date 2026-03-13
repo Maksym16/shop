13b7809ff7baf4a15b1a294507aba467280706f8
Maksym <mfedorenko16@gmail.com>
2026-03-12 22:20:43 -0400

    migrate db to neon

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -6,6 +6,7 @@ interface BillboardProps {
 }
 
  const Billboard: React.FC<BillboardProps> = ({data}) => {
+  if (!data) return null;
   return (
     <div className='p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden'>
       <div 
